e5f2d3d8f3974b14812c71d99b45937961781b14
DLi2004 <danielmli2023@gmail.com>
2023-09-09 19:41:44 -0400

    copy pasted basic tutorial

---
+++ main.py
@@ -0,0 +1,27 @@
+import asyncio
+
+from viam.robot.client import RobotClient
+from viam.rpc.dial import Credentials, DialOptions
+
+
+async def connect():
+    creds = Credentials(
+        type='robot-location-secret',
+        payload='3rn6lwpqwh5xzra3sy33x8ul94b5s20zf66ghe6v9bny577z')
+    opts = RobotClient.Options(
+        refresh_interval=0,
+        dial_options=DialOptions(credentials=creds)
+    )
+    return await RobotClient.at_address('robot7-main.wkhwbmlw2g.viam.cloud', opts)
+
+
+async def main():
+    robot = await connect()
+
+    print('Resources:')
+    print(robot.resource_names)
+
+    await robot.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
