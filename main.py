952ecd003a359cb8947d1ca0e800c34c102e922b
DLi2004 <danielmli2023@gmail.com>
2023-09-10 06:52:46 -0400

    final commit

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ import asyncio
 
 from viam.robot.client import RobotClient
 from viam.rpc.dial import Credentials, DialOptions
-from viam.components.camera import Camera
-from viam.components.board import Board
-from viam.components.base import Base
 from viam.services.vision import VisionClient
+from viam.components.base import Base
 
-import numpy as np
-import open3d as o3d
+# 
+SECRET = "3rn6lwpqwh5xzra3sy33x8ul94b5s20zf66ghe6v9bny577z"
 
 async def connect():
     creds = Credentials(
         type='robot-location-secret',
-        payload='3rn6lwpqwh5xzra3sy33x8ul94b5s20zf66ghe6v9bny577z')
+        payload=SECRET)
     opts = RobotClient.Options(
         refresh_interval=0,
         dial_options=DialOptions(credentials=creds)
@@ -22,39 +20,31 @@ async def connect():
 
 
 async def main():
-    spinNum = 10  # when turning, spin the motor this much
-    straightNum = 300  # when going straight, spin motor this much
     numCycles = 200  # how many times to repeat the main loop
-    vel = 500  # go this fast when moving motor
 
     # Connect to robot client and set up components
     robot = await connect()
     base = Base.from_robot(robot, "base")
-    camera = Camera.from_robot(robot, "webcam")
-
-    data, _ = await camera.get_point_cloud()
-
-    # write the point cloud into a temporary file
-    with open("/tmp/pointcloud_data.pcd", "wb") as f:
-        f.write(data)
-    pcd = o3d.io.read_point_cloud("/tmp/pointcloud_data.pcd")
-    points = np.asarray(pcd.points)
-
-    print(points)
 
     # Grab the vision service for the detector
-    # detector = VisionClient.from_robot(robot, "happy")
+    detector = VisionClient.from_robot(robot, "happy")
 
-    # # Main loop. Detect the baby
-    # for _ in range(numCycles):
-    #     # make sure that your camera name in the app matches "my-camera"
-    #     detections = await detector.get_detections_from_camera("my-webcam")
-    #     found = False
-    #     for d in detections:
-    #         if d.confidence > 0.8:
-    #             if d.class_name.lower() == "baby":
-    #                 print(f"Baby found at {d.x}, {d.y}")
+    # Main loop. Detect the baby
+    for _ in range(numCycles):
+        # make sure that your camera name in the app matches "my-camera"
+        detections = await detector.get_detections_from_camera("cam")
+        for d in detections:
+            if d.confidence > 0.8:
+                if d.class_name.lower() == "baby":
+                    print(f"Baby found at {d.x}, {d.y}!")
+                    # move the robot in the direction of the baby
+                    if d.x < 0.4:
+                        await base.spin(velocity=100, angle=80)
+                    elif d.x > 0.6:
+                        await base.spin(velocity=100, angle=-80)
+                    await base.move_straight(velocity=100, distance=100)
 
+    # Pause then close the connection
     await asyncio.sleep(100)
     await robot.close()
 
